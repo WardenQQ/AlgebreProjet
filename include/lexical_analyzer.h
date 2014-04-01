da43a7069a908e7c9d83305a53f7fe5c091038e3
WardenQQ <WardenQQ@server.fake>
2014-04-01 10:53:02 +0200

    Simplifier lexical_analyze.{c,h}

--- include/lexical_analyzer.h
+++ include/lexical_analyzer.h
@@ -1,53 +1,53 @@
-#ifndef _LEXICAL_ANALYZER_H_
-#define _LEXICAL_ANALYZER_H_
+#ifndef _TOKENS_H_
+#define _TOKENS_H_
 
-#define NAME_MAX 256
+#define STRING_MAX 256
 
 enum token_type {
-  TOKEN_EOF,
-  TOKEN_ID,
-  TOKEN_NUMBER,
-  // tokens for each function
-  TOKEN_MATRIX,
-  TOKEN_ADDITION,
-  TOKEN_SUB,
-  TOKEN_MULT,
-  TOKEN_MULT_SCAL,
-  TOKEN_EXPO,
-  TOKEN_TRANSPOSE,
-  TOKEN_DETERMINANT,
-  TOKEN_INVERT,
-  TOKEN_SOLVE,
-  TOKEN_RANK,
-  TOKEN_SPEEDTEST,
-  TOKEN_QUIT,
-  // tokens for punctuation
-  TOKEN_COMMA,
-  TOKEN_COLON,
-  TOKEN_LEFT_PARENTHESE,
-  TOKEN_RIGHT_PARENTHESE,
-  TOKEN_LEFT_BRACKET,
-  TOKEN_RIGHT_BRACKET,
+  TOK_ERROR = -1,
+  // Terminal tokens
+  TOK_COLON,
+  TOK_COMMA,
+  TOK_EOF,
+  TOK_NEWLINE,
+  TOK_SEMI_COLON,
+  TOK_LEFT_PARENTHESE,
+  TOK_RIGHT_PARENTHESE,
+  TOK_LEFT_BRACKET,
+  TOK_RIGHT_BRACKET,
+  TOK_NUMBER,
+  TOK_ID,
+  TOK_ADDITION,
+  TOK_DETERMINANT,
+  TOK_EXPO,
+  TOK_INVERT,
+  TOK_MATRIX,
+  TOK_MULT,
+  TOK_MULT_SCAL,
+  TOK_QUIT,
+  TOK_RANK,
+  TOK_SOLVE,
+  TOK_SPEEDTEST,
+  TOK_SUB,
+  TOK_TRANSPOSE
 };
 
 struct token_id {
   enum token_type type;
-  char name[NAME_MAX];
+  char name[STRING_MAX];
 };
 
 struct token_number {
   enum token_type type;
-  float value;
+  double value;
 };
 
 union token {
   enum token_type type;
-
   struct token_number number;
-
   struct token_id id;
 };
 
-union token get_next_token();
+union token gettoken();
 
-#endif // _LEXICAL_ANALYZER_H_
+#endif //_TOKENS_H_
